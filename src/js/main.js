f4fe228c5d20d4c85bc56d5ff9a68879637b619e
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-20 17:02:39 -0500

    created a buisness card mock up

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+import $ from 'jquery';
+import token from "./token.js";
+
+function gather(data, callback) {
+    $.ajax({
+        url: `https://api.github.com/users/TylerLaurenceau`,
+        dataType: "json",
+        headers: {
+            "Authorization": `token ${token}`
+        },
+        success: placeInfo
+    })
+};
+gather();
+
+function placeInfo(data) {
+    $(".info").append(`<ul class = "currentInfo">
+                        <li>Name: ${data.name}</li>
+                        <li><a href = "${data.html_url}">Github URL</a></li>
+                        <li><a href = "https://www.youtube.com/channel/UC5n6RslnzgMRV5AyXXSw6qA">YouTube Channel</a>
+                        <li>Email: ${data.email}</li>
+                        <li>Phone Number: (470)269-5150</li>
+                        <li>Location: ${data.location}</li>
+                        </ul>`)
+    placeAboutMe(data);
+}
+
+function placeAboutMe(data) {
+    $(".aboutMe").append(`<div>${data.bio}</div>`);
+    picture(data);
+}
+function picture(data){
+  console.log(data);
+  $(".picture").html(`<img src = "${data.avatar_url}"/>`);
+}
